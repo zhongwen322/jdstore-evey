357df462bd3fe2b6c2ed59202b2a042ba2e09eb3
evey <zhongwen322@gmail.com>
2017-02-10 00:02:52 +1300

    fix bug

--- db/schema.rb
+++ db/schema.rb
@@ -28,10 +28,10 @@ ActiveRecord::Schema.define(version: 20170209080130) do
   create_table "orders", force: :cascade do |t|
     t.integer  "total",            default: 0
     t.integer  "user_id"
-    t.integer  "billing_name"
-    t.integer  "billing_address"
-    t.integer  "shipping_name"
-    t.integer  "shipping_address"
+    t.string   "billing_name"
+    t.string   "billing_address"
+    t.string   "shipping_name"
+    t.string   "shipping_address"
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 u = User.new
-u.email = "admin@test.com"
+u.email = "test@gmail.com"
 
 u.password = "123456"
 
