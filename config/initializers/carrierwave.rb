926d25e7d93a6fb6b8d5c28f23b492694c59d3fd
evey <zhongwen322@gmail.com>
2017-02-13 23:38:21 +1300

    connect heroku with s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,9 @@
-CarrierWave.configure do|config|
-  config.fog_provider = 'fog/aws'
- config.fog_credentials = {
-  provider:              'AWS',
-  aws_access_key_id:     'AKIAJD4UZGSAMIFP7H2Q',
-  aws_secret_access_key: 'VmOJ1PmBqMWMbCtltsyiiKV7Pg64a0y97WM9oVlY',
-  region:                'US West (N. California)',
-  host:                  's3.example.com',
-  endpoint:              'https://s3.example.com'
-
-
- }
- config.fog_directory = 'seanlinzhongwen'
- config.fog_public = 'false'
- config.fog_attributes = {'Cache-Control' => "max-age=#{365.to_i}" }
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => "AKIAJD4UZGSAMIFP7H2Q",
+      :aws_secret_access_key  => "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",
+      :region                 => 'us-west-1' # Change this for different AWS region. Default is 'us-east-1'
+  }
+      config.fog_directory  = "seanlinzhongwen"
 end
