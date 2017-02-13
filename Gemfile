91e9200ddb5cd07fd39c57bae41e8023e6270140
evey <zhongwen322@gmail.com>
2017-02-13 22:36:54 +1300

    add s3

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'simple_form'
 gem 'font-awesome-rails'
 gem 'carrierwave'
 gem 'mini_magick'
+gem 'fog'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,13 @@
+CarrierWave.configure do|config|
+  config.fog_provider = 'fog/aws'
+ config.fog_credentials = {
+  provider:              'AWS',
+  aws_access_key_id:     'AKIAJD4UZGSAMIFP7H2Q',
+  aws_secret_access_key: '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',
+  region:                'US West (N. California)',
+
+ }
+ config.fog_directory = 'seanlinzhongwen'
+ config.fog_public = 'false'
+ config.fog_attributes = {'Cache-Control' => "max-age=#{365.to_i}" }
+end
