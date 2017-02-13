b779007e6f545c6708fe8c12a9563696bb59a389
evey <zhongwen322@gmail.com>
2017-02-13 20:55:00 +1300

    try to connect with s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,14 @@
 CarrierWave.configure do|config|
+  config.fog_provider = 'fog/aws'
  config.fog_credentials = {
   provider:              'AWS',
-  aws_access_key_id:     'AWS_ACCESS_KEY',
-  aws_secret_access_key: 'AWS_SECRET_KEY',
-  region:                'region-name',
+  aws_access_key_id:     'AKIAJD4UZGSAMIFP7H2Q',
+  aws_secret_access_key: 'VmOJ1PmBqMWMbCtltsyiiKV7Pg64a0y97WM9oVlY',
+  region:                'US West (N. California)',
   host:                  's3.example.com',
-  endpoint:              'https://s3.example.com'
+  endpoint:              'http://seanlinzhongwen.s3-website-us-west-1.amazonaws.com'
  }
- config.fog_directory = 'name of the bucket'
+ config.fog_directory = 'seanlinzhongwen'
  config.fog_public = 'false'
  config.fog_attributes = {'Cache-Control' => "max-age=#{365.to_i}" }
 end
