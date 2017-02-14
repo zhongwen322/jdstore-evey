007a240575432fe2096839f7772b6d2957018260
evey <zhongwen322@gmail.com>
2017-02-15 10:27:25 +1300

    change carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,10 @@
-CarrierWave.configure do|config|
- config.fog_credentials = {
-  provider:              'AWS',
-  aws_access_key_id:     'XXX',
-  aws_secret_access_key: 'YYY',
-  region:                'us-west-1',
-  host:                  's3.example.com',
-  endpoint:              'https://s3.example.com'
- }
- config.fog_directory = 'seanlinzhongwen123'
- config.fog_public = 'false'
- config.fog_attributes = {'Cache-Control' => "max-age=#{365.to_i}" }
+
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => "XXX",
+      :aws_secret_access_key  => "YYY",
+      :region                 => 'us-west-1' # Change this for different AWS region. Default is 'us-east-1'
+  }
+      config.fog_directory  = "seanlinzhongwen123"
 end
