03d64f54774569c70c629f6034c353e07c7dc1ef
evey <zhongwen322@gmail.com>
2017-02-15 17:24:49 +1300

    delete key

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,8 @@
-
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => "AKIAJK3B6NRTY4DN5H5Q",
-      :aws_secret_access_key  => "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",
+      :aws_access_key_id      => “XXX",
+      :aws_secret_access_key  => “YYY",
       :region                 => 'us-west-1' # Change this for different AWS region. Default is 'us-east-1'
   }
       config.fog_directory  = "seanlinzhongwen123"
