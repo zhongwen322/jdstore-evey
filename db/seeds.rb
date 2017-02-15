1d3f5b3f1465acee29e04ea1848a29e4de0b0c97
evey <zhongwen322@gmail.com>
2017-02-16 12:49:43 +1300

    change admin account

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 u = User.new
-u.email = "test1@gmail.com"
+u.email = "test2@gmail.com"
 
 u.password = "123456"
 
