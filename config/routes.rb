ef3551f3f1b13cda51be954ddb73437eb69a64cc
sean lin <seanlin0918@gmail.com>
2017-02-18 02:19:38 +1000

    finish search function, tested

--- config/routes.rb
+++ config/routes.rb
@@ -17,12 +17,12 @@ Rails.application.routes.draw do
 
   resources :products do
 
-    member do
-      post :add_to_cart
-    end
-
     collection do
       get :search
+    end
+
+    member do
+      post :add_to_cart
     end
 
   end
