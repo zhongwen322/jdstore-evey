5e34849cc9fd5605d504bd715b369ca12d75daef
zhongwen322 <zhongwen322@gmail.com>
2017-02-19 22:51:14 +1300

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => "AKIAJK3B6NRTY4DN5H5Q",
-      :aws_secret_access_key  => "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",
+      :aws_access_key_id      => "xxx",
+      :aws_secret_access_key  => "yyy",
       :region                 => 'us-west-1' # Change this for different AWS region. Default is 'us-east-1'
   }
       config.fog_directory  = "seanlinzhongwen123"
